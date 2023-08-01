9e13c18487292495d21cfcf730d458a7fd909537
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-07-31 23:12:11 -0400

    Submits working

--- scripts.js
+++ scripts.js
@@ -1,23 +1,70 @@
 let myLibrary = ['book1', 'book2'];
 
-function Book(name, read) {
-  this.name = name;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary() {
-    let newBook = new Book();
-    myLibrary.push(newBook);
+function showBooks() {
+    const booksList = document.getElementById('books');
+    
+    document.querySelectorAll('.bookDiv').forEach(e => e.remove());
+
+    myLibrary.forEach((book) => {
+        let newBookDiv = document.createElement('div');
+        let newBook = document.createElement('h1');
+        let newReadButton = document.createElement('button');
+        let newDeleteButton = document.createElement('button');
+
+        newBookDiv.className = 'bookDiv';
+        newBookDiv.id = 'bookDiv';
+        newBook.innerText = book;
+        newBook.id = book;
+        newBook.className = 'bookTitle';
+        newReadButton.innerText = 'Read';
+        newReadButton.id = 'read-' + book;
+        newDeleteButton.innerText = 'Delete';
+        newDeleteButton.id = 'del-' + book;
+
+        newBookDiv.appendChild(newBook);
+        newBookDiv.appendChild(newReadButton);
+        newBookDiv.appendChild(newDeleteButton);
+        booksList.appendChild(newBookDiv);
+    })
 }
 
+function addBookToLibrary(title, author, pages, read) {
+    event.preventDefault();
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+    showBooks();
+}
+
+function getValues(event){
+    event.preventDefault();
+    let title = this.title.value;
+    let author = this.author.value;
+    let pages = this.pages.value;
+    let read = this.read.value;
+    addBookToLibrary(title, author, pages, read);
+ }	
+
 window.onload = function() {
-    const button = document.getElementById('btn');
+    const newBookButton = document.getElementById('newBookBtn');
+    const newBookForm = document.getElementById('addForm');
+
     const booksList = document.getElementById('books');
     let notLoaded = true;
 
-    button.addEventListener('click', () => {
-        console.log(notLoaded);
-      });
+    newBookButton.addEventListener('click', function() {
+        newBookForm.style.display = 'block';
+        newBookButton.style.display = 'none';
+    });
+
+    newBookForm.addEventListener("submit", getValues);
 
     if (notLoaded) {
         myLibrary.forEach((book) => {
